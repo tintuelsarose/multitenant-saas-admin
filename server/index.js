61cf94062e2d0516e8156d3542e05877f528c69c
tintuelsarose <tintuelsarose@gmail.com>
2026-05-06 15:07:22 +0530

    feat: backend scaffold with Express, MongoDB connection

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Multi-Tenant SaaS API is running' });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
